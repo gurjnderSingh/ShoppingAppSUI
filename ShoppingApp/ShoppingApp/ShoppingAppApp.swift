3a8dc1d9b5baac2d0e1166e6de1cf2d8c375fec4
gurjnderSingh <gurjindersingh663@gmail.com>
2023-10-30 11:08:15 +0530

    inital commit

---
+++ ShoppingApp/ShoppingApp/ShoppingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShoppingAppApp.swift
+//  ShoppingApp
+//
+//  Created by Gurjinder Singh on 30/10/23.
+//
+
+import SwiftUI
+
+@main
+struct ShoppingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
